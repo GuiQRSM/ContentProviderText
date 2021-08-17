b6f3c3d9ae8b5fdadc19f78c01bca7d3f99dd133
GuiQRSM <endbeob15@gmail.com>
2021-08-17 16:43:26 -0300

    It implements the instantiations in the methods of the 'ContentProvider', for activating the processes in the URL.

--- app/src/main/java/com/littletoth/contentprovidertext/database/NoteProvider.kt
+++ app/src/main/java/com/littletoth/contentprovidertext/database/NoteProvider.kt
@@ -2,49 +2,94 @@ package com.littletoth.contentprovidertext.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
 import android.net.Uri
+import android.provider.BaseColumns._ID
+import com.littletoth.contentprovidertext.database.NotesDatabaseHelper.Companion.TABLE_NOTES
 
 class NoteProvider : ContentProvider() {
 
     private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotesDatabaseHelper
 
     override fun onCreate(): Boolean {
       mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
       mUriMatcher.addURI(AUTHOTRITY, "notes", NOTES)
         mUriMatcher.addURI(AUTHOTRITY,"notes/#", NOTES_BY_ID)
-
+      if (context != null) {dbHelper = NotesDatabaseHelper(context as Context)
+      }
         return true
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID) {
+           val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffected: Int = db.delete(TABLE_NOTES,"$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffected
+        }else {
+            throw UnsupportedSchemeException("URL invalida para exclusão")
+        }
+
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+    override fun getType(uri: Uri): String? = throw UnsupportedSchemeException()
+
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+        if(mUriMatcher.match(uri) == NOTES) {
+           val db: SQLiteDatabase = dbHelper.writableDatabase
+            val id: Long = db.insert(TABLE_NOTES, null, values)
+            val insertUri = Uri.withAppendedPath(BASE_URI, id.toString())
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return insertUri
+        } else {
+            throw UnsupportedSchemeException("URL invalida para inserção")
+        }
     }
 
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+       return when {
+           mUriMatcher.match(uri) == NOTES -> {
+               val db: SQLiteDatabase = dbHelper.writableDatabase
+               val cursor = db.query(TABLE_NOTES, projection, selection, selectionArgs, null, null, sortOrder)
+               cursor.setNotificationUri(context?.contentResolver, uri)
+               cursor
+           }
+           mUriMatcher.match(uri) == NOTES_BY_ID -> {
+               val db: SQLiteDatabase = dbHelper.writableDatabase
+               val cursor: Cursor = db.query(TABLE_NOTES, projection, "$_ID = ?", arrayOf(uri.lastPathSegment), null, null, sortOrder)
+               cursor.setNotificationUri((context as Context).contentResolver, uri)
+               cursor
+           }
+           else -> {
+               throw UnsupportedSchemeException("Uri não implrmentada.")
+           }
+       }
     }
 
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID) {
+          val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect: Int = db.update(TABLE_NOTES, values, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        }else {
+            throw UnsupportedSchemeException("Uri não implrmentada.")
+        }
     }
 
     companion object {
